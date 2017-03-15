378d8373c2e845b0a81008f9cf5febdae950f4f3
Larson <troylars@amazon.com>
2017-03-15 07:39:01 -0400

    Added setup.py/setup.cfg

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='progressinsight',
+  packages=['progressinsight'],
+  version='0.1.0',
+  description='Real-time workflow progress tracking',
+  author='Troy Larson',
+  author_email='troylar@gmail.com',
+  url='hhttps://github.com/troylar/ProgressInsight',
+  download_url='https://github.com/troylar/ProgressInsight/tarball/0.1',
+  keywords=['metrics', 'logging', 'aws', 'progress', 'workflow'],
+  install_requires=['redis', 'fluentmetrics'],
+  classifiers=[],
+)
